e53e63977c53522897f44aa2c3164c78ccf32fb4
Flavio Augusto Oliveira Costa <flavio1@ice.ufjf.br>
2022-08-17 15:45:53 -0300

    Adicionado código javascript para geração do mapa

---
+++ js/script.js
@@ -0,0 +1,55 @@
+var z = 1;
+const container = document.getElementById("container");
+function vazio(x){
+	for(var i = 0; i < x; i++){
+		const div = document.createElement("div");
+		div.setAttribute('id', z);
+		div.classList.add("vazio");
+		container.appendChild(div);
+		z++;
+	}	
+}
+function estrada(x){
+	for(var i = 0; i < x; i++){
+		const div = document.createElement("div");
+		div.setAttribute('id', z);
+		div.classList.add("estrada");
+		container.appendChild(div);
+		z++;
+	}
+}
+function cidade(){
+	const div = document.createElement("div");
+	div.setAttribute('id', z);
+	div.classList.add("cidade");
+	container.appendChild(div);
+	z++;
+}
+function ginasio(){
+	const div = document.createElement("div");
+	div.setAttribute('id', z);
+	div.classList.add("jogador");
+	container.appendChild(div);
+	z++;
+}
+
+vazio(23);ginasio();vazio(4);ginasio();vazio(16);estrada(1);vazio(4);estrada(1);vazio(16);estrada(1);vazio(4);estrada(1);vazio(16);estrada(1);vazio(4);estrada(1);vazio(4);ginasio();estrada(4);ginasio();vazio(6);estrada(1);vazio(4);estrada(1);vazio(4);estrada(1);vazio(4);estrada(1);vazio(6);estrada(1);vazio(4);estrada(1);vazio(4);estrada(1);vazio(4);estrada(1);vazio(6);ginasio();estrada(4);ginasio();estrada(4);ginasio();estrada(4);ginasio();estrada(3);ginasio();vazio(7);estrada(1);vazio(13);estrada(1);vazio(7);estrada(1);vazio(13);estrada(1);vazio(7);estrada(1);vazio(13);estrada(1);vazio(7);estrada(1);vazio(3);ginasio();estrada(9);ginasio();vazio(7);estrada(1);vazio(3);estrada(1);vazio(17);estrada(1);vazio(3);estrada(1);vazio(17);estrada(1);vazio(3);estrada(1);vazio(17);estrada(1);vazio(3);estrada(1);vazio(17);estrada(1);vazio(3);estrada(1);vazio(12);ginasio();estrada(4);ginasio();estrada(3);ginasio();estrada(4);ginasio();estrada(4);ginasio();vazio(23);
+
+
+
+
+
+
+
+//for{
+	//Z; func(4); #; func2(16);
+	//func2(44);
+	//func2(6); #; func(4);# func2(5);
+	//func2(44);
+	//#; func(4); A; func(4); #; func(4); #; func(4); X;
+	//func2(44);
+	//func2(8); #; func(12); #
+	//func2(88);
+	//X; func(4); #; func(4); #; func(4); #; func(4); R;
+
+//}
